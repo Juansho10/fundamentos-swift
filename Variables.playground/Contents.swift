d2055dc3a8beaa59bd37d134cd82c326a774901b
JP <jupayala@nequi.com>
2025-05-19 17:09:52 -0500

    Primer commit

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,39 @@
+let firtConstante = 10
+var secondConstante = 20
+
+secondConstante = 30
+
+var firtString = "Hello World"
+var firtBool = true
+
+var firtDouble = 10.5
+
+var otherString: String = "Hello World"
+var otherBool: Bool = true
+
+var otherDouble: Double = 10.5
+
+var n1: Double = 10
+var n2: Double = 20
+
+var sum = n1 / n2
+
+var s1 = "Hello"
+var s2 = " World"
+
+var concatenacion = s1 + " " + s2
+
+var b1 = true
+var b2 = true
+
+var or = b1 || b2
+var and = b1 && b2
+
+let name = "Juan"
+let greetings = "Hola \(name). Estas en el curso de Swift, tu eres el mejor"
+print(greetings)
+
+// "Numero de caracteres en el nombre: "
+
+let numberOfCharactersInName = name.count
+print("Numero de caracteres en el nombre: \(numberOfCharactersInName)") 
